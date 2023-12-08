973eadd7bb856ce70b3ffaf9d0f70b5678bbc1b1
Ignaciofabian93 <98052832+Ignaciofabian93@users.noreply.github.com>
2023-12-08 18:22:56 -0300

    files

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.API_KEY,
-  authDomain: import.meta.env.AUTH_DOMAIN,
-  projectId: import.meta.env.PROJECT_ID,
-  storageBucket: import.meta.env.STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-  appId: import.meta.env.APP_ID,
+  apiKey: "AIzaSyA428v9MVYhJc2fQZ2WvsKqr9uZCdRiZLo",
+  authDomain: "chatapp-8b90d.firebaseapp.com",
+  projectId: "chatapp-8b90d",
+  storageBucket: "chatapp-8b90d.appspot.com",
+  messagingSenderId: "14108660781",
+  appId: "1:14108660781:web:2917dc54cc5d1b0ee5d4a4",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import MainLayout from "../Layouts/MainLayout";
 import Textfield from "../components/Textfield/Textfield";
 import Button from "../components/Buttons/Button";
 import useMessage from "../hooks/useMessage";
+import { ThemeContext } from "../context/themeContext";
 
 const Home = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
   const { newMessage, handleMessage, sendMessage } = useMessage();
   return (
     <MainLayout>
       <h1>Home</h1>
       <Textfield value={newMessage} onChange={handleMessage} />
-      <Button text="Send" onClick={sendMessage} />
+      <Button text="Send" onClick={toggleTheme} />
     </MainLayout>
   );
 };
