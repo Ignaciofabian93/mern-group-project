a08dc4a9b6656f8536a4c85c8f365709368c3518
David Martínez Tapia <dmmtapia.ux@gmail.com>
2023-12-12 17:30:37 -0300

    logo navbar

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import useMessage from "../../hooks/useMessage";
 import { IconMoon } from "../Icons/Moon";
 import { IconSun } from "../Icons/Sun";
 import { Logo } from "../Icons/Logo";
 
 export const Navbar = ({ darkMode, toogleDarkMode }) => {
-  const { user } = useMessage();
+  
 
   return (
-    <div className="bg-gray-500 justify-between flex px-10 py-2 dark:bg-[#1E1F22] transition duration-500 ">
+    <div className="bg-gray-500 justify-between border-gray-300 border-b-2 flex px-10 py-2 dark:bg-[#1E1F22] transition duration-500 dark:border-[#313338]  ">
       <div className="flex gap-2 items-center">
         <Logo />
         <p className="text-white font-semibold dark:text-white transition duration-500">
