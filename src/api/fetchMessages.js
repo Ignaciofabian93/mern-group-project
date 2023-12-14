fb1b85d9980b077b356615cfc58a8ee26107ac83
Ignaciofabian93 <98052832+Ignaciofabian93@users.noreply.github.com>
2023-12-14 18:11:14 -0300

    pointing to server

--- src/api/fetchMessages.js
+++ src/api/fetchMessages.js
@@ -10,3 +10,15 @@ export const getMessages = async () => {
     throw new Error(`Error: ${error}`);
   }
 };
+
+
+export const saveMessages = async (user, content, room) => {
+  try {
+    const response = await api.post("/messages", {user, content, room});
+    const data = await response.data;
+    console.log("data: ", data);
+    return data
+  } catch (error) {
+    throw new Error(`Error: ${error}`)
+  }
+}
