532361de9537e028f30e3f60f2730c68977033eb
Ignaciofabian93 <98052832+Ignaciofabian93@users.noreply.github.com>
2023-12-20 22:00:54 -0300

    url service

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const url = "http://localhost:8080";
-// export const url = "https://mern-group-project-services-dev-rfnb.1.us-1.fl0.io";
+// export const url = "http://localhost:8080";
+export const url = "https://mern-group-project-services-dev-rfnb.1.us-1.fl0.io";
 
 export const api = axios.create({
   baseURL: url,
